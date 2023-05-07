b3d20b8f6c60723bbfa0b7989bb6e7662630033e
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2023-05-07 15:20:55 +0300

    refactor: login Razor view

---
+++ DailyDine/Models/Account/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyDine.Models.Account
+{
+    public class LoginModel
+    {
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
