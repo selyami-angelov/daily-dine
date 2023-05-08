437b39b4e587a54f8cca29c9803c8e034c1d1c52
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2023-05-08 11:17:46 +0300

    feat: product service

---
+++ DailyDine.Core/Contracts/IProductService.cs
@@ -0,0 +1,31 @@
+﻿using DailyDine.Core.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyDine.Core.Contracts
+{
+    /// <summary>
+    /// Manipulates product data
+    /// </summary>
+    public interface IProductService
+    {
+        /// <summary>
+        /// Gets all products
+        /// </summary>
+        /// <returns>List of products</returns>
+        Task<IEnumerable<ProductDto>> GetAll();
+
+        /// <summary>
+        /// Add new product
+        /// </summary>
+        /// <param name="productDto">Product model</param>
+        /// <returns></returns>
+        Task Add(ProductDto productDto);
+
+        Task Delete(Guid id);
+    }
+}
