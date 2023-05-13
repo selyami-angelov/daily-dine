02abcd1192aa65ddd97b0b1f36e6c7e1c4fce003
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2023-05-13 16:15:40 +0300

    refactor: dtos dir

---
+++ DailyDine.Core/Dtos/CategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyDine.Core.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ DailyDine.Core/Dtos/ProductDto.cs
@@ -0,0 +1,36 @@
+﻿using DailyDine.Infrastructure.Data.Entities;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DailyDine.Core.Dtos
+{
+    /// <summary>
+    /// Product model
+    /// </summary>
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(150, MinimumLength = 40)]
+        public string Description { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string CreatedById { get; set; } = null!;
+
+        public DateTime EditedDate { get; set; }
+
+        public string EditedById { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public byte[] ProductImage { get; set; } = Array.Empty<byte>();
+    }
+}
